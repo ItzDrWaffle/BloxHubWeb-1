715ca166ba8cb23a7be74d1464d00d84a531a4c4
ItzDrWaffle <74496806+ItzDrWaffle@users.noreply.github.com>
2021-05-16 11:14:54 -0400

    Update loader.lua

--- script/loader.lua
+++ script/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/BloxHubFixed/main/MainCode")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/BloxHubFixed/main/MainCode2")()
